64fc5b3a13ff4620459d2aa54f167d7372b7ec71
Matt Hinchliffe <matt@maketea.co.uk>
2014-10-13 16:49:50 +0100

    Remove separate welcome controller, use search index as root

--- config/routes.rb
+++ config/routes.rb
@@ -5,12 +5,11 @@ Rails.application.routes.draw do
   get 'suggestion', to: 'suggestion#index'
   post 'suggestion', to: 'suggestion#create'
 
-  get 'search', to: 'search#index'
-  get 'search/results'
+  get 'search', to: 'search#results'
 
   get 'venue', to: 'venue#index'
   get 'venue/:id', to: 'venue#show'
 
-  root 'welcome#index'
+  root 'search#index'
 
 end
