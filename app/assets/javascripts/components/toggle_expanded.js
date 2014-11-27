802e6bbf0cc1931a8a0fd794bee186366be61e99
Matt Hinchliffe <matt@maketea.co.uk>
2014-11-27 14:02:07 +0000

    Add site header partial and styles, add animations and easing libs, add white and dark SVG sprites

--- app/assets/javascripts/components/toggle_expanded.js
+++ app/assets/javascripts/components/toggle_expanded.js
@@ -1,17 +1,20 @@
 function ToggleExpanded($target) {
   this.$target = $target;
-  this.$toggle = $target.siblings("button[aria-controls='#" + $target.attr("id") + "']");
+  this.$toggle = $("[aria-controls='#" + $target.attr("id") + "']");
 }
 
 ToggleExpanded.prototype.init = function() {
-  this.$target.addClass("is-closed");
-
   this.$toggle
     .attr("aria-expanded", false)
     .on("click", $.proxy(this._onClick, this));
 };
 
 ToggleExpanded.prototype._onClick = function() {
-  this.$target.toggleClass("is-closed is-open");
-  this.$toggle.attr("aria-expanded", this.$target.hasClass("is-open"));
+  var expanded = this.$target.hasClass("is-open");
+
+  this.$target
+    .addClass("is-" + (expanded ? "closed" : "open"))
+    .removeClass("is-" + (expanded ? "open" : "closed"));
+
+  this.$toggle.attr("aria-expanded", expanded);
 };
