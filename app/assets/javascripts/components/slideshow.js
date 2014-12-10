833665e50c3502c0048af7743f2615292603fe0c
Matt Hinchliffe <matt@maketea.co.uk>
2014-12-10 00:23:08 +0000

    Smooth out slideshow and add image loading on demand

--- app/assets/javascripts/components/slideshow.js
+++ app/assets/javascripts/components/slideshow.js
@@ -5,13 +5,13 @@ function Slideshow($target) {
 }
 
 Slideshow.prototype.init = function() {
-  this.$btnNext = $("<button class='Slideshow-btn Slideshow-btn--next'>Next slide</button>");
   this.$btnPrev = $("<button class='Slideshow-btn Slideshow-btn--prev'>Prev slide</button>");
+  this.$btnNext = $("<button class='Slideshow-btn Slideshow-btn--next'>Next slide</button>");
 
-  this.$btnNext.on("click", $.proxy(this.next, this)).appendTo(this.$target);
   this.$btnPrev.on("click", $.proxy(this.prev, this)).appendTo(this.$target);
+  this.$btnNext.on("click", $.proxy(this.next, this)).appendTo(this.$target);
 
-  this.$target.on("keyup", $.proxy(this._keypress, this));
+  this.$target.attr("tabindex", 0).on("keyup", $.proxy(this._keypress, this));
 
   this.to(0);
 
@@ -19,23 +19,23 @@ Slideshow.prototype.init = function() {
 };
 
 Slideshow.prototype.to = function(x) {
-  var next = this._loop(x + 1);
-  var prev = this._loop(x - 1);
   var current = this._loop(x);
+  var prev = this._loop(current - 1);
+  var next = this._loop(current + 1);
 
   this.$frames.removeClass("is-next is-prev is-current");
 
-  this.$frames.eq(next).addClass("is-next");
-  this.$frames.eq(prev).addClass("is-prev");
-  this.$frames.eq(current).addClass("is-current");
-};
-
-Slideshow.prototype.next = function() {
-  this.to(this._current() + 1);
+  this._preload(this.$frames.eq(current).addClass("is-current"));
+  this._preload(this.$frames.eq(prev).addClass("is-prev"));
+  this._preload(this.$frames.eq(next).addClass("is-next"));
 };
 
 Slideshow.prototype.prev = function() {
   this.to(this._current() - 1);
+};
+
+Slideshow.prototype.next = function() {
+  this.to(this._current() + 1);
 };
 
 Slideshow.prototype._keypress = function(e) {
@@ -56,4 +56,16 @@ Slideshow.prototype._current = function() {
 Slideshow.prototype._loop = function(x) {
   var maximum = this.$frames.length - 1;
   return x > maximum ? 0 : (x < 0 ? maximum : x);
+};
+
+Slideshow.prototype._preload = function($frame) {
+  var $img = $frame.prop("loaded", true).find("img");
+
+  if ($img.hasClass("is-loaded")) return;
+
+  $img
+    .attr("src", $img.attr("data-src"))
+    .attr("alt", $img.attr("data-alt"))
+    .removeAttr("data-src data-alt")
+    .addClass("is-loaded");
 };
