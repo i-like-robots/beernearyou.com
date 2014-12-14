b8d14c74129c4312d6164f0223b2ac98d4cfcda9
Matt Hinchliffe <matt@maketea.co.uk>
2014-12-14 12:49:29 +0000

    Output useful venue information, add basic venue details layout

--- app/helpers/venue_helper.rb
+++ app/helpers/venue_helper.rb
@@ -1,2 +1,11 @@
 module VenueHelper
+
+  def formatted_opening_hours(timeframe)
+    hours = timeframe['open'].map do |open|
+      open['renderedTime']
+    end
+
+    formatted = "#{timeframe['days']}: #{hours.join(', ')}"
+  end
+
 end
