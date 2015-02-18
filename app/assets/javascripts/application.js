247afc2285091e108fc1898a7c26cba654a17b74
Matt Hinchliffe <matt@maketea.co.uk>
2015-02-18 11:29:22 +0000

    Setup Bower to manage 3rd party dependencies

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,11 +1,14 @@
 //= require jquery2
 //= require ./config
 //= require ./support
+//= require ./manifest
 //= require_tree ./utils
 //= require_tree ./components
 //= require_tree ./views
 
 window.support = new Support().init();
+
+document.documentElement.classList.remove("no-js");
 
 $(document).ready(function() {
   var views = $("html").data("jsView").split(" ").concat("global");
