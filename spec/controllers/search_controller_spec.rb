7c6e5c982c27f9f465271d523edb443f436bb08f
Matt Hinchliffe <matt@maketea.co.uk>
2015-04-30 13:28:17 +0100

    Add condition to check whether search location is within area covered by the app

--- spec/controllers/search_controller_spec.rb
+++ spec/controllers/search_controller_spec.rb
@@ -7,39 +7,62 @@ RSpec.describe SearchController, :type => :controller do
 
   describe '#results' do
 
-    let(:venues) { build_list(:venue, 5, :with_location) }
-    let(:origin) { [ 51.514150, -0.227108 ] }
+    context 'within range' do
 
-    before(:each) do
-      expect(subject).to receive(:find_venues_with_origin).with(origin).and_return(venues)
-    end
-
-    context 'search by location' do
-
-      let(:query) { { lat: origin.first, lng: origin.last } }
-
-      it 'finds nearby venues with given coordinates' do
-        get :results, query
-        expect(assigns(:live)).to eq(true)
-        expect(assigns(:venues)).to eq(venues)
-        expect(assigns(:origin)).to eq(origin)
-      end
-
-    end
-
-    context 'search by postcode' do
-
-      let(:query) { { postcode: 'W12 7TQ' } }
+      let(:origin) { [ 51.514150, -0.227108 ] }
+      let(:venues) { build_list(:venue, 5, :with_location) }
 
       before(:each) do
-        expect(subject).to receive(:create_origin_with_params).with(hash_including(query)).and_return(origin)
+        expect(subject).to receive(:find_venues_with_origin).with(origin).and_return(venues)
       end
 
-      it 'finds nearby venues with given coordinates' do
+      context 'search by location' do
+
+        let(:query) { { lat: origin.first, lng: origin.last } }
+
+        it 'finds nearby venues with given coordinates' do
+          get :results, query
+          expect(assigns(:live)).to eq(true)
+          expect(assigns(:venues)).to eq(venues)
+          expect(assigns(:origin)).to eq(origin)
+          expect(assigns(:within_bounds)).to eq(true)
+        end
+
+      end
+
+      context 'search by postcode' do
+
+        let(:query) { { postcode: 'W12 7TQ' } }
+
+        before(:each) do
+          expect(subject).to receive(:create_origin_with_params).with(hash_including(query)).and_return(origin)
+        end
+
+        it 'finds nearby venues with given coordinates' do
+          get :results, query
+          expect(assigns(:live)).to_not eq(true)
+          expect(assigns(:venues)).to eq(venues)
+          expect(assigns(:origin)).to eq(origin)
+          expect(assigns(:within_bounds)).to eq(true)
+        end
+
+      end
+
+    end
+
+    context 'outside range' do
+
+      let(:origin) { [ 75.7667, 99.7833 ] }
+      let(:query)  { { lat: origin.first, lng: origin.last } }
+
+      before(:each) do
+        expect(subject).not_to receive(:find_venues_with_origin)
+      end
+
+      it 'sets out of bounds flag' do
         get :results, query
-        expect(assigns(:live)).to_not eq(true)
-        expect(assigns(:venues)).to eq(venues)
-        expect(assigns(:origin)).to eq(origin)
+        expect(assigns(:venues)).to eq(nil)
+        expect(assigns(:within_bounds)).to eq(false)
       end
 
     end
